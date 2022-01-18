0fb6b86d24f2cd3d513caca17fa30a65dab20731
Jimmy <dm.a.uvarov@gmail.com>
2022-01-18 10:28:52 -0500

    commit tree 456a5f750b8be98877268e73b34d373467d2082d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+//1
+//  window.addEventListener('load', function(e) {
+//    // const p = document.getElementById(`text`);
+//   //   p.innerText = 'This is really cool!';
+//     console.log("The DOM has loaded");
+//     /*if (document.readyState !== 'loading' ){*/
+//         afterLoadedDom();
+//     /*}*/
+
+    
+//   });
+//2
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById("text").textContent = "This is really cool!"
+});
+
+//3
+// document.addEventListener('readystatechange', ()=> afterLoadedDom());
+
+
+// function afterLoadedDom(){
+//     const p = document.getElementById(`text`);
+//    p.innerText = 'This is really cool!';
+// //    p.innerText = 'JavaScript is so cool. It lets me add text to my page programmatically.'
+//     console.log(p);
+
+// }
